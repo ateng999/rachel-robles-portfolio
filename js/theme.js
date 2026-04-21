691cc876642411601386e3f88f24fac14970e534
rachel.robles1 <rachel.robles1@trans-cosmos.co.jp>
2026-04-21 12:03:52 +0800

    edit

--- js/theme.js
+++ js/theme.js
@@ -75,6 +75,25 @@
 	/*----------------------------------------------------*/
 
 	/*----------------------------------------------------*/
+	/* Glow-btn
+	/*----------------------------------------------------*/
+	document.querySelectorAll('.glass-btn').forEach(btn => {
+		btn.addEventListener('mousemove', e => {
+			const rect = btn.getBoundingClientRect();
+			const x = e.clientX - rect.left - rect.width / 2;
+			const y = e.clientY - rect.top - rect.height / 2;
+
+			btn.style.transform = `translate(${x * 0.1}px, ${y * 0.1}px)`;
+		});
+		btn.addEventListener('mouseleave', () => {
+			btn.style.transform = '';
+		});
+	});
+	/*----------------------------------------------------*/
+	/*  End  glow
+	/*----------------------------------------------------*/
+	
+	/*----------------------------------------------------*/
 	/* Start Magnific Pop Up
 	/*----------------------------------------------------*/
 	if ($('.img-gal').length > 0) {
