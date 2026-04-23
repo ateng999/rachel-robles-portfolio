785b49f73e8d9ad424d98eeaaf28119bc4e3fe98
rachel.robles1 <rachel.robles1@trans-cosmos.co.jp>
2026-04-23 21:34:40 +0800

    edit

--- js/mail-script.js
+++ js/mail-script.js
@@ -1,31 +1,31 @@
-    // -------   Mail Send ajax
+emailjs.init("-f8jm2PtPzhOZ2_BO");
 
-     $(document).ready(function() {
-        var form = $('#myForm'); // contact form
-        var submit = $('.submit-btn'); // submit button
-        var alert = $('.alert-msg'); // alert div for show alert message
+const form = document.getElementById('contactForm');
 
-        // form submit event
-        form.on('submit', function(e) {
-            e.preventDefault(); // prevent default form submit
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
 
-            $.ajax({
-                url: 'mail.php', // form action url
-                type: 'POST', // form submit method get/post
-                dataType: 'html', // request type html/json/xml
-                data: form.serialize(), // serialize form data
-                beforeSend: function() {
-                    alert.fadeOut();
-                    submit.html('Sending....'); // change submit button text
-                },
-                success: function(data) {
-                    alert.html(data).fadeIn(); // fade in response data
-                    form.trigger('reset'); // reset form
-                    submit.attr("style", "display: none !important");; // reset submit button text
-                },
-                error: function(e) {
-                    console.log(e)
-                }
-            });
-        });
-    });
+    const btn = form.querySelector('.btn_submit');
+    btn.innerHTML = "Sending...";
+    btn.disabled = true;
+
+    try {
+        await emailjs.sendForm(
+        "service_okoya8e",
+        "template_k5tqszq",
+        form
+        );
+
+        btn.innerHTML = "✔ Sent";
+        form.reset();
+
+    } catch (error) {
+        console.error("EmailJS Error:", error);
+        btn.innerHTML = "❌ Failed";
+    }
+
+    setTimeout(() => {
+        btn.innerHTML = "<span>Send Message</span>";
+        btn.disabled = false;
+    }, 2000);
+});
