ead38e51954f2b079a79838d08ddbb4c100a4d9c
rachel.robles1 <rachel.robles1@trans-cosmos.co.jp>
2026-04-28 09:26:08 +0800

    add

--- js/theme.js
+++ js/theme.js
@@ -316,4 +316,23 @@
 			},
 		},
 	});
+
+	const backToTop = document.getElementById("backToTop");
+
+	// SHOW / HIDE
+	window.addEventListener("scroll", () => {
+		if (window.scrollY > 300) {
+			backToTop.classList.add("show");
+		} else {
+			backToTop.classList.remove("show");
+		}
+	});
+
+	// SCROLL TO TOP
+	backToTop.addEventListener("click", () => {
+		window.scrollTo({
+			top: 0,
+			behavior: "smooth"
+		});
+	});
 })(jQuery);
